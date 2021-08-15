186263877d32f272c681705a675adf3ef68b8ddb
Elechim01 <68447158+Elechim01@users.noreply.github.com>
2021-08-15 18:35:20 +0200

    Initial Commit

---
+++ GlassMorphism/GlassMorphismApp.swift
@@ -0,0 +1,17 @@
+//
+//  GlassMorphismApp.swift
+//  GlassMorphism
+//
+//  Created by Michele Manniello on 15/08/21.
+//
+
+import SwiftUI
+
+@main
+struct GlassMorphismApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
